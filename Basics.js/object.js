3ac19593570feec3d04b411c7d4899deee03f0a7
Arvindkr-03 <arvind.bcastudent22.8111@cimage.in>
2025-04-13 20:55:38 +0000

    Added some new methods to access keys and values

--- Basics.js/object.js
+++ Basics.js/object.js
@@ -9,6 +9,15 @@ var obj={
     play:"Cricket",
     age:43
 }
-console.log(obj.name);
-console.log("Plays");
-console.log(obj.play);
+// console.log(obj.name);
+// console.log("Plays");          // Basic methods
+// console.log(obj.play);
+
+// console.log(obj['name']);
+// console.log("Plays");
+// console.log(obj['play']);
+// console.log(obj['age']);
+
+// Object.values(obj).forEach(v => console.log(v));        //new one
+
+Object.entries(obj).forEach(([k, v]) => console.log(`${k}: ${v}`));
