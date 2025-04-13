d10acf24141621fc3a298400ad260da680bf332f
Arvindkr-03 <arvind.bcastudent22.8111@cimage.in>
2025-04-13 20:35:14 +0000

    first object created with name obj

---
+++ Basics.js/object.js
@@ -0,0 +1,14 @@
+//Objects:
+//  Objects is a collection of key-value pairs,
+//  Keys are also known as properties that holds some values inside.
+//  It must be written inside the middle curly braces.
+//  It always stored as a string.
+
+var obj={
+    name:"Rohit Sharma",
+    play:"Cricket",
+    age:43
+}
+console.log(obj.name);
+console.log("Plays");
+console.log(obj.play);
