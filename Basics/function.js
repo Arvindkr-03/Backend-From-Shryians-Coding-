142d419f6d9ce752d73336f84c65db153c73ee0f
Arvindkr-03 <arvind.bcastudent22.8111@cimage.in>
2025-04-13 21:39:12 +0000

    folder name changed to BAsics

---
+++ Basics/function.js
@@ -0,0 +1,13 @@
+// A function is a reusable block of code that performs a specific task.
+// Group code together & reuse it multiple times in code.
+
+function user() {
+    console.log("Hello, Js Users");
+
+}
+
+function newUser(name) {
+    console.log("Hello, " + name + "!");
+}
+
+greetUser("Mohan");
