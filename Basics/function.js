2fb64dc22401efa624a97037baa8619f6644f227
Arvindkr-03 <arvind.bcastudent22.8111@cimage.in>
2025-04-13 22:04:42 +0000

    learn how function and how it call and return

--- Basics/function.js
+++ Basics/function.js
@@ -2,12 +2,22 @@
 // Group code together & reuse it multiple times in code.
 
 function user() {
-    console.log("Hello, Js Users");
-
+    // console.log("Hello, Js Users");
+    // console.log(user.length); // it returns 0. as lenght.
+    return "Boliye!";
 }
+user();
 
 function newUser(name) {
     console.log("Hello, " + name + "!");
+    console.log(newUser.length);
 }
+newUser("Mohan");
+// it returns 1 beacuse one parameter set length t0 1
 
-greetUser("Mohan");
+
+function sun() {
+    return "Sun rises in the morning!"
+}
+var ans = sun();
+console.log(ans);
